85fa583c9f63af4230ef7e71a99b7851e57c9651
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-02-20 19:45:51 +0100

    feat: update city class

--- models/city.py
+++ models/city.py
@@ -4,13 +4,14 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, Datetime, ForeignKey
 from sqlalchemy.orm import relationship
 
+
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
 
     __tablename__ = "cities"
     name = Column("name", String(128), nullable=False)
-    state_id = Column("state_id", String(60),  ForeignKey("states.id"), nullable=False)
+    state_id = Column("state_id", String(60),
+                      ForeignKey("states.id"), nullable=False)
 
-    state = relationship("State", back_populates="cities", cascade="all, delete-orphan")
-    
-
+    state = relationship("State", back_populates="cities",
+                         cascade="all, delete-orphan")
