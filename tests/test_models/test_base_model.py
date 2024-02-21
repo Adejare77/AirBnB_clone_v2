7ca91b03921a2307b65f14138a6a2053dcc61f16
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-02-21 12:30:52 +0100

    docs: add docstring to all methods [test_base_model.py]

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -12,35 +12,36 @@ class test_basemodel(unittest.TestCase):
     """ """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ initialization for Test case"""
         super().__init__(*args, **kwargs)
         self.name = 'BaseModel'
         self.value = BaseModel
 
     def setUp(self):
-        """ """
+        """set up method for test clase """
         pass
 
     def tearDown(self):
+        """ tear down method for test class"""
         try:
             os.remove('file.json')
         except:
             pass
 
     def test_default(self):
-        """ """
+        """ test for successful instantiation"""
         i = self.value()
         self.assertEqual(type(i), self.value)
 
     def test_kwargs(self):
-        """ """
+        """test for successfull instantiation with kwargs """
         i = self.value()
         copy = i.to_dict()
         new = BaseModel(**copy)
         self.assertFalse(new is i)
 
     def test_kwargs_int(self):
-        """ """
+        """test instantiation with int kwargs """
         i = self.value()
         copy = i.to_dict()
         copy.update({1: 2})
@@ -57,41 +58,41 @@ class test_basemodel(unittest.TestCase):
             self.assertEqual(j[key], i.to_dict())
 
     def test_str(self):
-        """ """
+        """checks string method of object"""
         i = self.value()
         self.assertEqual(str(i), '[{}] ({}) {}'.format(self.name, i.id,
                          i.__dict__))
 
     def test_todict(self):
-        """ """
+        """ test to_dict method"""
         i = self.value()
         n = i.to_dict()
         self.assertEqual(i.to_dict(), n)
 
     def test_kwargs_none(self):
-        """ """
+        """test instantiation with no kwarg """
         n = {None: None}
         with self.assertRaises(TypeError):
             new = self.value(**n)
 
     def test_kwargs_one(self):
-        """ """
+        """test instantiation with one kwarg """
         n = {'Name': 'test'}
         with self.assertRaises(KeyError):
             new = self.value(**n)
 
     def test_id(self):
-        """ """
+        """ test id attribute"""
         new = self.value()
         self.assertEqual(type(new.id), str)
 
     def test_created_at(self):
-        """ """
+        """created_at attribute test"""
         new = self.value()
         self.assertEqual(type(new.created_at), datetime.datetime)
 
     def test_updated_at(self):
-        """ """
+        """test updated_at atrribute """
         new = self.value()
         self.assertEqual(type(new.updated_at), datetime.datetime)
         n = new.to_dict()
