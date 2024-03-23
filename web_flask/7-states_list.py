180f6fd3fa05795d4f08dd81f73d74ffdd21ce15
Adejare <adejare.arasheed@gmail.com>
2024-03-23 18:35:45 +0100

    Web framework

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -16,14 +16,19 @@ def close(e):
 
 @app.route("/states_list", strict_slashes=False)
 def list_state():
-    state_dict = {k: v for k, v in all_objs.items()
+    """ list State and their Cities in sorted order """
+    # The all_objs is unsorted and contains all classes. value is state name
+    state_dict = {k: v.__dict__['name'] for k, v in all_objs.items()
                   if k.split(".")[0] == "State"}
-    return (state_dict)
-    sorted_state_dict = dict(sorted(state_dict.items(),
-                                    key=lambda item: item[1]))
-    return (sorted_state_dict)
-    return render_template('7-states_list.html', val=sorted_state_dict)
+    # The state_name_sort helps sort state using the name of the the state
+    state_name_sort = dict(sorted(state_dict.items(),
+                                  key=lambda item: item[1]))
+    # Since, state_name_sort has been sorted, we now need to have key: value
+    # where value is an instance. This means will have sorted all_objs
+    sorted_inst_state = {k: all_objs[k] for k in state_name_sort.keys()}
+
+    return render_template('7-states_list.html', states=sorted_inst_state)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
