5cda49cb650a5fd5292f3f34f8a01769f574e90c
Adejare <adejare.arasheed@gmail.com>
2024-03-23 20:49:52 +0100

    web framework

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,9 +3,16 @@
 
 from models import storage
 from flask import Flask, render_template
+from models.state import State
 
-all_objs = storage.all()
+all_objs = storage.all(State)
 app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def list_state():
+    """ list State and their Cities in sorted order """
+    return render_template('7-states_list.html', states=all_objs)
 
 
 @app.teardown_appcontext
@@ -14,21 +21,5 @@ def close(e):
     storage.close()
 
 
-@app.route("/states_list", strict_slashes=False)
-def list_state():
-    """ list State and their Cities in sorted order """
-    # The all_objs is unsorted and contains all classes. value is state name
-    state_dict = {k: v.__dict__['name'] for k, v in all_objs.items()
-                  if k.split(".")[0] == "State"}
-    # The state_name_sort helps sort state using the name of the the state
-    state_name_sort = dict(sorted(state_dict.items(),
-                                  key=lambda item: item[1]))
-    # Since, state_name_sort has been sorted, we now need to have key: value
-    # where value is an instance. This means will have sorted all_objs
-    sorted_inst_state = {k: all_objs[k] for k in state_name_sort.keys()}
-
-    return render_template('7-states_list.html', states=sorted_inst_state)
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
