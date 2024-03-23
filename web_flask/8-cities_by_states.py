d42c7ed757afcd9269b1ee08fc8bd4843bb43427
Adejare <adejare.arasheed@gmail.com>
2024-03-23 22:16:06 +0100

    Web Framework

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Starts a Flask web Application """
+
+from models import storage
+from flask import Flask, render_template
+from models.state import State
+
+app = Flask(__name__)
+states = storage.all(State)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def list_state():
+    """ list State and their Cities in sorted order """
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown(e):
+    """ close session """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
