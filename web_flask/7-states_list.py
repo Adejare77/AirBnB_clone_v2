dbf2557cb4b7e2a3b74b03fbc1738726665995fa
Adejare <adejare.arasheed@gmail.com>
2024-03-24 11:44:18 +0100

    Web Framework

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,19 +6,19 @@ from flask import Flask, render_template
 from models.state import State
 
 app = Flask(__name__)
+states = storage.all(State)
 
 
 @app.route("/states_list", strict_slashes=False)
 def list_state():
     """ list State and their Cities in sorted order """
-    states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
-@app.teardown_appcontext
-def close(e):
-    """ close session """
-    storage.close()
+# @app.teardown_appcontext
+# def close(e):
+#     """ close session """
+#     storage.close()
 
 
 if __name__ == '__main__':
